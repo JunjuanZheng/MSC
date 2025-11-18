947c40be35ff92656b1f4d0ff1cc7047927f7b38
JunjuanZheng <junjuanzheng20160616@gmail.com>
2025-11-18 17:20:25 +0800

    Implement monocle3 analysis for scRNA-seq data
    This script implements monocle3 analysis for single-cell RNA sequencing data, including data preprocessing, trajectory inference, and visualization of gene expression trends over pseudotime.

---
+++ Monocle3.R
@@ -0,0 +1,273 @@
+library(paletteer)
+library(Seurat)
+library(monocle3)
+library(dplyr)
+library(BiocParallel)
+library(scop)
+PrepareEnv()
+
+load('/mnt2/wanggd_group/zjj/BGCscRNA/MSC_Duan/Data/Integration20250829/20250829_3SamplesMSC_FastMNNIntegration_FindClusters_objIntegrated.Rdata')
+scRNA=objIntegrated
+scRNA$celltype=scRNA$MSC_Clusters
+scRNA[["RNA"]]<-as(object=scRNA[["RNA"]],Class="Assay")  #Assay5转成Assay3
+
+register(MulticoreParam(workers=4,progressbar=TRUE))
+pdf(file.path(projectPath, "Output/Integration20250829","objIntegrated_Monocle3_Result.pdf"), height = 10, width = 10)
+expression_matrix <- GetAssayData(scRNA, assay = 'RNA',slot = 'counts')
+cell_metadata <- scRNA@meta.data 
+gene_annotation <- data.frame(gene_short_name = rownames(expression_matrix))
+rownames(gene_annotation) <- rownames(expression_matrix)
+##构建细胞轨迹
+cds <- new_cell_data_set(expression_matrix,
+                         cell_metadata = cell_metadata,
+                         gene_metadata = gene_annotation)
+
+###monocle3分析前预处理
+# 归一化/预处理数据
+# 这一步使用的PCA分析，dim数代表纳入的PCA数量
+cds <- preprocess_cds(cds, num_dim = 25,norm_method = c("none"))
+# 这个函数用于确认设定的dim数是否足够代表主要变异
+plot_pc_variance_explained(cds)
+#选择一个能够保证大部分Variance都纳入的PCA componets值。
+# 可选(去批次处理)
+cds <- align_cds(cds, alignment_group = "orig.ident")
+#.降维并可视化
+# 降维聚类，可选择UMAP、PCA或者TSNE
+coloursBy = 'celltype'
+cds <- reduce_dimension(cds,reduction_method='UMAP',preprocess_method = 'PCA')
+###基因/轨迹共定位
+#这里可以看基因和轨迹的共定位情况
+#modelgenes <- "KRAS"
+#plot_cells(cds,
+#           cell_size=1.5,group_label_size=4,
+#           genes=modelgenes,
+#           label_cell_groups=FALSE,
+#           show_trajectory_graph=FALSE)
+
+#细胞聚类
+cds <- cluster_cells(cds) #cluster your cells
+plot_cells(cds, color_cells_by = 'partition')#coloursBy)
+
+cds0 = cds
+
+###Seurat的umap分布数据替换monocle3的umap数据
+cds.embed <- cds@int_colData$reducedDims$UMAP
+int.embed <- Embeddings(scRNA, reduction = "umap.fastMNN")
+int.embed <- int.embed[rownames(cds.embed),]
+cds@int_colData$reducedDims$UMAP <- int.embed
+
+
+###.轨迹分析
+# 轨迹推断
+cds <- learn_graph(cds,
+				   verbose=T,
+                   use_partition=F,  #默认是T，T时是顾及全局的情况
+                   learn_graph_control=list(minimal_branch_len=20,#在图修剪过程中要保留的分支直径路径的最小长度。默认值是10。
+                                            euclidean_distance_ratio=10#生成树中两个末端节点的欧氏距离与生成树上允许连接的任何连接点之间的最大距离之比。默认值为1。
+                      ))
+				   
+p1 = plot_cells(cds, 
+           color_cells_by = coloursBy,
+           label_groups_by_cluster=FALSE,
+           cell_size=1,group_label_size=4,
+           label_leaves= F, # 是否显示不同细胞结局
+           label_branch_points=F, # 是否显示不同的分支节点
+           trajectory_graph_color='#023858',
+           trajectory_graph_segment_size = 1)
+print(p1)
+###定义起点-轨迹可视化
+#接下来按照CytoTRACE2中的结果设定细胞发育的起点。
+#定义root cell, 推断拟时方向
+# 网页自定
+#cds <- order_cells(cds)  
+# 代码定
+# a helper function to identify the root principal points:
+## 指定初始细胞群和细胞亚群的列名
+time_bin="C3"
+get_earliest_principal_node <- function(cds, time_bin=time_bin, coloursBy) {
+  # 确保 `coloursBy` 变量存在并正确
+  if (!coloursBy %in% colnames(colData(cds))) {
+    stop(paste("The column", coloursBy, "does not exist in colData(cds)"))
+  }
+  # 找到符合条件的细胞 ID
+  cell_ids <- which(colData(cds)[, coloursBy] == time_bin)
+  if(length(cell_ids) == 0) {
+    stop(paste("No cells found for time_bin:", time_bin))
+  }
+  # 计算最近的主成分节点
+  closest_vertex <- cds@principal_graph_aux[["UMAP"]]$pr_graph_cell_proj_closest_vertex
+  closest_vertex <- as.matrix(closest_vertex[colnames(cds), ])
+  root_pr_nodes <- igraph::V(principal_graph(cds)[["UMAP"]])$name[
+					as.numeric(names(which.max(table(closest_vertex[cell_ids,]))))
+					]
+  return(root_pr_nodes)
+}
+# 获取最近的主成分节点
+root_pr_nodes <- get_earliest_principal_node(cds, time_bin=time_bin, coloursBy=coloursBy)
+# 使用获得的节点顺序细胞
+cds <- order_cells(cds, root_pr_nodes=root_pr_nodes)
+
+#2D可视化
+p2 = plot_cells(cds, label_cell_groups = F, 
+           color_cells_by = "pseudotime", 
+           label_branch_points = F, 
+           label_roots =F, # 显示根
+           label_leaves =F,
+           graph_label_size = 0, 
+           cell_size=2, 
+           trajectory_graph_color='black',
+           trajectory_graph_segment_size = 2)
+print(p2)
+
+p002 = plot_cells(cds, color_cells_by = "pseudotime", label_cell_groups = FALSE, 
+                  label_leaves = FALSE, trajectory_graph_color = "purple", cell_size = 0.8,
+                  label_branch_points = FALSE, 
+                  label_roots = F) + 
+  theme(legend.position="right", 
+        # legend.key.size =  unit(0.2, "inches"), 
+        legend.title = element_blank(), 
+        legend.text = element_text(size = 16), 
+        text = element_text(size = 16), axis.text = element_text(size = 16)) + 
+  # theme(panel.border = element_rect(fill=NA,color= "black", size=1, linetype= "solid"))  + 
+  tidydr::theme_dr()+
+  scale_color_gradientn(colours = c("#0088C3","#6FBCDB","#BCE5F1","#F2EFDA","#FFC482","#FF7B4C","#FF5831"))
+print(p002)
+
+p111=plot_cells(cds, color_cells_by = "pseudotime", label_cell_groups = FALSE, 
+              label_leaves = FALSE,  label_branch_points = FALSE,
+              trajectory_graph_color = "white",
+              cell_size=0.3) +
+  theme_classic()+
+  theme(axis.text.x = element_text(size=10,color="black"),
+        axis.text.y = element_text(size=10,color="black"))+
+  scale_color_manual(values  = c("red","blue"))
+  
+pdata<-p111$data
+
+pp01 = ggplot(pdata,aes(x=data_dim_1,data_dim_2,color=cell_color))+
+  geom_point(size=0.5)+
+  labs(x="umap_1",y="umap_2",color="pseudotime",title="pseudotime")+
+  scale_color_gradientn(colours  = c("#96b6e2","#dbe5f5","#f6c7c0","#eda69d","#ce564f"))+
+  theme_classic()+
+  theme(axis.text.x = element_text(size=10,color="black"),
+        axis.text.y = element_text(size=10,color="black"),
+        plot.title = element_text(hjust = 0.5)  # This centers the title
+  )	
+print(pp01)
+  
+pp02 = ggplot(pdata,aes(x=celltype,y=cell_color,fill=celltype))+
+  geom_violin(scale=T,width=1)+
+  scale_fill_manual(values =c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FDBF6F","#FF7F00"))+
+  theme_classic()+
+  labs(x="",y="pseudotime")+
+  theme(axis.text.x = element_text(size=10,color="black"),
+        axis.text.y = element_text(size=10,color="black"))
+print(pp02)
+#计算每个origin下的pse组成
+pdata <- pdata %>%
+  mutate(cell_color_category = case_when(
+    cell_color >= 0 & cell_color < 10 ~ "pse 0-10",
+    cell_color >= 10 & cell_color < 20 ~ "pse 10-20",
+    cell_color >= 20 & cell_color <= 27 ~ "pse 20-30",
+    TRUE ~ NA_character_  # 对于不在上述范围内的值，标记为 NA
+  ))
+# 计算每个 origin 分组下的 subtype 组成百分比
+percent_data <- pdata %>%
+  group_by(celltype, cell_color_category) %>%
+  summarise(count = n()) %>%
+  mutate(percentage = count / sum(count) * 100)
+# 使用 ggplot2 绘制柱形图
+ppp1=ggplot(percent_data, aes(x = celltype, y = percentage, fill = cell_color_category)) +
+  geom_bar(stat = "identity", position = "stack",color="black") +
+  labs(title = "          Pseudotime",
+       x = "",
+       y = "Percentage",
+       fill = "Subtype") +
+  scale_fill_manual(values =c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FDBF6F","#FF7F00"))+
+  theme_classic()+theme(axis.text.x = element_text(size=10,color="black"),
+                        axis.text.y = element_text(size=10,color="black"))
+print(ppp1)
+pseudotime(cds)
+cds$monocle3_pseudotime <- pseudotime(cds)
+data.pseudo <- as.data.frame(colData(cds))
+
+pppp = ggplot(data.pseudo, aes(monocle3_pseudotime, reorder(celltype, monocle3_pseudotime, median), fill = celltype)) +
+  geom_boxplot() +
+  scale_fill_manual(values =c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FDBF6F","#FF7F00"))
+print(pppp)
+
+
+###monocle3常见图谱
+genes <- graph_test(cds, neighbor_graph="principal_graph", cores=10)
+
+save(cds,genes, file = file.path(projectPath, "Data/Integration20250829","objIntegrated_Monocle3_cds.genes.Rdata"))
+
+load(file.path(projectPath, "Data/Integration20250829","objIntegrated_Monocle3_cds.Rdata"))
+
+#挑选top10画图展示
+uedeGenes <- genes %>% top_n(n=80, morans_I) %>%
+  pull(gene_short_name) %>% as.character()
+  
+uedeGenes <- genes %>% subset(q_value < 0.05 & morans_I > 0.005) %>%
+  pull(gene_short_name) %>% as.character()
+  
+TopGenes <- uedeGenes[
+    !grepl("^MT-", uedeGenes) &        # 过滤线粒体基因
+    !grepl("^RPL\\d+", uedeGenes) &    # 过滤核糖体大亚基蛋白基因
+    !grepl("^RPS\\d+", uedeGenes) &    # 过滤核糖体小亚基蛋白基因
+    !grepl("^ENSCAFG", uedeGenes)      # 过滤Ensembl犬基因ID
+]  
+  
+#基因表达趋势图
+pseuPlot = plot_genes_in_pseudotime(cds[TopGenes[1:10],], 
+                         min_expr=0.5, ncol = 2)
+print(pseuPlot)
+						 
+pseuPlot00 = plot_genes_in_pseudotime(cds[TopGenes[1:10],], color_cells_by="celltype", 
+                         min_expr=0.5, ncol = 2)	
+print(pseuPlot00)
+						 
+#FeaturePlot图
+feaPlot=plot_cells(cds, genes=TopGenes, show_trajectory_graph=FALSE,
+           label_cell_groups=FALSE,  label_leaves=FALSE)
+print(feaPlot)
+dev.off()
+
+###
+# Filter significant genes
+pdf(file.path(projectPath, "Output/Integration20250829","objIntegrated_Monocle3_Result02_TopOrderedGenes.pdf"), height = 10, width = 10)
+deg_genes <- subset(genes, q_value < 0.05)
+deg_genes_Ordered = deg_genes[order(deg_genes$morans_I, decreasing = TRUE), ]
+#head()
+# Plot top dynamic gene
+#top_gene <- rownames(deg_genes[which.max(deg_genes$morans_I), ])
+top_gene <- rownames(deg_genes_Ordered)[1:20]
+
+TopGenes <- top_gene[
+    !grepl("^MT-", top_gene) &        # 过滤线粒体基因
+    !grepl("^RPL\\d+", top_gene) &    # 过滤核糖体大亚基蛋白基因
+    !grepl("^RPS\\d+", top_gene) &    # 过滤核糖体小亚基蛋白基因
+    !grepl("^ENSCAFG", top_gene)      # 过滤Ensembl犬基因ID
+]  
+rowData(cds)$gene_short_name <- rownames(rowData(cds))
+
+pseuPlot001 = plot_genes_in_pseudotime(cds[TopGenes,], 
+                         min_expr=0.5, ncol = 2,color_cells_by = "pseudotime")
+print(pseuPlot001)
+dev.off()
+library(RColorBrewer)
+# 获取 Paired 调色板
+paired_colors <- c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FDBF6F","#FF7F00")#brewer.pal(length(unique(colData(cds)$celltype)), "Paired")
+color_map <- setNames(paired_colors, levels(unique(colData(cds)$celltype)) )
+
+temp= c('COL1A1','COL1A2','COL11A1','SERPINE1','ACTB','ACTA2')
+pseuPlot0001 = plot_genes_in_pseudotime(cds[temp,], 
+                         min_expr=0.5, ncol = 2,color_cells_by = "pseudotime")
+pseuPlot00002 = plot_genes_in_pseudotime(cds[temp,], 
+                         min_expr=0.5, ncol = 2,color_cells_by = "celltype") +
+               scale_color_manual(values = color_map)						 
+print(pseuPlot0001)
+print(pseuPlot0002)
+dev.off()
+
+###
